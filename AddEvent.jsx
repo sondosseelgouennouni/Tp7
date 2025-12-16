898752c42f205d47b6d4673a0eb23c34206fd304
sondosseelgouennouni <opalson2025@gmail.com>
2025-12-17 00:57:41 +0100

    Refactor AddEvent component for clarity and consistency

--- AddEvent.jsx
+++ AddEvent.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-export default function AddEvent({ onAdd, onUpdate, onClose, editEv}) {
+export default function AddEvent({ onAdd, onUpdate, onClose, editEvent }) {
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
   const [lieu, setLieu] = useState("");
@@ -16,37 +16,66 @@ export default function AddEvent({ onAdd, onUpdate, onClose, editEv}) {
       setType(editEvent.type || "atelier");
     }
   }, [editEvent]);
-  const handleSubmit = (e) => { e.preventDefault();
-    const snd = {title,date,lieu,description,type,};
 
-    if (editEv) {
-      onUpdate({ ...snd, id: editEv.id, favorite: editEv.favorite });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const eventData = {
+      title,
+      date,
+      lieu,
+      description,
+      type,
+    };
+
+    if (editEvent) {
+      onUpdate({ ...eventData, id: editEvent.id, favorite: editEvent.favorite });
     } else {
-      onAdd({ ...snd, id: Date.now(), favorite: false });
+      onAdd({ ...eventData, id: Date.now(), favorite: false });
     }
 
     setTitle("");
     setDate("");
     setLieu("");
     setDescription("");
-    setType("Conference");
+    setType("atelier");
+
     onClose();
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input placeholder="Titre" value={title} onChange={(e) => setTitle(e.target.value)}/>
-      <input placeholder="Lieu" value={lieu} onChange={(e) => setLieu(e.target.value)}/>
-      <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+      <input
+        placeholder="Titre"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <input
+        placeholder="Lieu"
+        value={lieu}
+        onChange={(e) => setLieu(e.target.value)}
+      />
+
+      <input
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+
       <select value={type} onChange={(e) => setType(e.target.value)}>
         <option>atelier</option>
         <option>conférence</option>
         <option>personnel</option>
         <option>autre</option>
       </select>
-      <textarea placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
-      <button>{editEv ? "Modifier" : "Enregistrer"}</button>
+
+      <textarea
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <button>{editEvent ? "Modifier" : "Enregistrer"}</button>
     </form>
   );
 }
-
